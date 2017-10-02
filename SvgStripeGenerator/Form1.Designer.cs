5bfc0faaec12b269421f664a0b1143c9a7d418c9
Gregor Schulte <gregor.schulte@uni-tuebingen.de>
2017-10-02 15:35:20 +0200

    mod

--- SvgStripeGenerator/Form1.Designer.cs
+++ SvgStripeGenerator/Form1.Designer.cs
@@ -32,6 +32,12 @@
             this.MyTextBoxHeight = new System.Windows.Forms.TextBox();
             this.MyTextBoxWidth = new System.Windows.Forms.TextBox();
             this.MyTextBoxStripes = new System.Windows.Forms.TextBox();
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.colorDialog2 = new System.Windows.Forms.ColorDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             this.SuspendLayout();
             // 
             // MyBottonWrite
@@ -50,6 +56,7 @@
             this.MyTextBoxHeight.Name = "MyTextBoxHeight";
             this.MyTextBoxHeight.Size = new System.Drawing.Size(100, 26);
             this.MyTextBoxHeight.TabIndex = 1;
+            this.MyTextBoxHeight.Text = "100";
             // 
             // MyTextBoxWidth
             // 
@@ -57,6 +64,7 @@
             this.MyTextBoxWidth.Name = "MyTextBoxWidth";
             this.MyTextBoxWidth.Size = new System.Drawing.Size(100, 26);
             this.MyTextBoxWidth.TabIndex = 2;
+            this.MyTextBoxWidth.Text = "206.088";
             // 
             // MyTextBoxStripes
             // 
@@ -64,12 +72,42 @@
             this.MyTextBoxStripes.Name = "MyTextBoxStripes";
             this.MyTextBoxStripes.Size = new System.Drawing.Size(100, 26);
             this.MyTextBoxStripes.TabIndex = 3;
+            this.MyTextBoxStripes.Text = "2";
+            this.MyTextBoxStripes.TextChanged += new System.EventHandler(this.MyTextBoxStripes_TextChanged);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Black;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(13, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(26, 26);
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.Color.White;
+            this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox2.Location = new System.Drawing.Point(12, 44);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(26, 27);
+            this.pictureBox2.TabIndex = 5;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // colorDialog2
+            // 
+            this.colorDialog2.Color = System.Drawing.Color.White;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(278, 244);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.MyTextBoxStripes);
             this.Controls.Add(this.MyTextBoxWidth);
             this.Controls.Add(this.MyTextBoxHeight);
@@ -77,6 +115,8 @@
             this.Name = "Form1";
             this.Text = "SvgWiter";
             this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -88,6 +128,10 @@
         private System.Windows.Forms.TextBox MyTextBoxHeight;
         private System.Windows.Forms.TextBox MyTextBoxWidth;
         private System.Windows.Forms.TextBox MyTextBoxStripes;
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.ColorDialog colorDialog2;
     }
 }
 
--- SvgStripeGenerator/Form1.cs
+++ SvgStripeGenerator/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace SvgStripeGenerator
 {
@@ -17,19 +18,69 @@ namespace SvgStripeGenerator
             InitializeComponent();
         }
 
-        string MySVGstart = "";
-        string MyySVGcontent = "";
-        string MySVGend = "";
+        string MySVGstart = "<svg  xmlns=\"http://www.w3.org/2000/svg\"\r\nxmlns:xlink=\"http://www.w3.org/1999/xlink\" width=\"297mm\" height=\"210mm\"> \n\r";
+        string MySVGcontent = "\n\r";
+        string MySVGend = "</svg>";
+        string MySVG = "";
+        string path = @"c:\temp\MyTest.svg";
+        bool color = true;
+        string bw = "#000000";
+
 
 
         private void MyBottonWrite_Click(object sender, EventArgs e)
         {
+
+            for (int i = 1; i< int.Parse(MyTextBoxStripes.Text)+1; i++)
+            {
+                if (color) { bw = HexConverter(colorDialog1.Color); } else { bw = HexConverter(colorDialog2.Color); }
+                MySVGcontent = MySVGcontent +"<rect x=\""+ (i*(double.Parse(MyTextBoxWidth.Text)/int.Parse(MyTextBoxStripes.Text))).ToString() +"mm\" y=\"10mm\" height=\"100mm\" width=\""+ ((double.Parse(MyTextBoxWidth.Text) / int.Parse(MyTextBoxStripes.Text))).ToString() + "mm\" style=\"stroke:"+ bw +"; stroke-width:0.1mm; fill: "+ bw +"  \"/> \n\r";
+                color = !color;
+            }
+
+            MySVG = MySVGstart + MySVGcontent + MySVGend;
+            File.WriteAllText(path, MySVG);
+
             Console.Beep();
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
 
+        }
+
+        private void MyTextBoxStripes_TextChanged(object sender, EventArgs e)
+        {
+            int parsedValue;
+            if (!int.TryParse(MyTextBoxStripes.Text, out parsedValue))
+            {
+                MessageBox.Show("This is a number only field");
+                return;
+            }
+            else if (parsedValue<1 || parsedValue>100  )
+            {
+                MessageBox.Show("Values have to be between 1 and 100   ");
+                return;
+
+            }
+            
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            colorDialog1.ShowDialog();
+            pictureBox1.BackColor = colorDialog1.Color;
+        }
+
+        private static String HexConverter(System.Drawing.Color c)
+        {
+            return "#" + c.R.ToString("X2") + c.G.ToString("X2") + c.B.ToString("X2");
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            colorDialog2.ShowDialog();
+            pictureBox2.BackColor = colorDialog2.Color;
         }
     }
 }
